e93f1a1ae9dd9d6c9d9dfdfa17fe480592439834
splumms <saraplummer@protonmail.com>
2023-11-03 14:26:45 -0300

    content: add footer links

--- components/site-footer.vue
+++ components/site-footer.vue
@@ -2,15 +2,17 @@
   <footer class="site-footer">
     <div class="grid-center">
       <div class="col-4_sm-6_mi-12 contact">
-        <a href="">Tel: 902-542-5869</a>
-        <a href="">wolfvillelegion@gmail.com</a>
-        <a href="">Facebook</a>
-        <a class="address" href="">310 Main St, <br>Wolfville, NS B4P 1C4</a>
+        <a href="tel:902-542-5869">Tel: 902-542-5869</a>
+        <a href="mailto:wolfvillelegion@gmail.com">wolfvillelegion@gmail.com</a>
+        <a href="https://www.facebook.com/WolfvilleLegion/">Facebook</a>
+        <a class="address" href="https://www.google.com/maps/place/Royal+Canadian+Legion+Branch+74/@45.0919997,-64.3599627,17z/data=!3m1!4b1!4m6!3m5!1s0x4b5855a0a4c92d8f:0x377436fca35ad7d9!8m2!3d45.0919959!4d-64.3573878!16s%2Fg%2F1tf6ls_y?entry=ttu">310 Main St, <br>Wolfville, NS B4P 1C4</a>
       </div>
       <div class="col-4_sm-6_mi-12 footer-nav">
         <a href="/rentals">Rentals</a>
-        <a href="">By-laws</a>
-        <a href="">Newsletter</a>
+        <a href="/branch-by-laws.pdf">Branch By-laws</a>
+        <a href="/NS-NU_COMMAND_BY-LAWS_MASTER_Revs_up_to_April_2020.pdf">Provincial By-laws</a>
+        <a href="/POLICY_AND_GUIDELINES_BOOK_-_25_Feb_20.pdf">Provincial Policy</a>
+        <a href="/legion-quarterly-vol-1-issue-1-april-2023.pdf">Newsletter</a>
       </div>
       <div class="col-4_sm-12 ctas">
         <Button v-for="(cta, index) in footerData.ctas"
@@ -97,6 +99,11 @@ export default {
   z-index: 5;
   a {
     color: white;
+    &:hover {
+      &:before {
+        width: 0;
+      }
+    }
   }
 }
 .contact, .ctas, .footer-nav {
