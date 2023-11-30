b8b657263c4d21d4820133abacb5e0dd52843408
splumms <saraplummer@protonmail.com>
2023-11-30 16:46:43 -0400

    content: update sponsor sliders

--- components/blocks/slider-block-2.vue
+++ components/blocks/slider-block-2.vue
@@ -11,8 +11,8 @@
           <img :src="'/images/poppy-flower.svg'">
         </div>
         <div class="text-wrapper">
-          <h6>FCC Agrispirit Fund</h6>
-          <p class="description">New Kitchen</p>
+          <h6>New Horizons for Seniors Program</h6>
+          <p class="description">Structural, sports bar and Accessible washroom upgrades</p>
         </div>
       </div>
       <div class="slidewrapper">
@@ -20,8 +20,8 @@
           <img :src="'/images/poppy-flower.svg'">
         </div>
         <div class="text-wrapper">
-          <h6>Kings Vision Grant</h6>
-          <p class="description">Lower level bar, washrooms and kitchen</p>
+          <h6>Legion Capital Assistance Program</h6>
+          <p class="description">Structural Re-enforcement</p>
         </div>
       </div>
       <div class="slidewrapper">
@@ -30,7 +30,7 @@
         </div>
         <div class="text-wrapper">
           <h6>Access-Ability Program, NS</h6>
-          <p class="description">Power Door, ramp, washroom accesability upgrades, elevator shaft</p>
+          <p class="description">Accessible entryway and elevator-well</p>
         </div>
       </div>
       <div class="slidewrapper">
@@ -38,8 +38,17 @@
           <img :src="'/images/poppy-flower.svg'">
         </div>
         <div class="text-wrapper">
-          <h6>Recreation Facility Development</h6>
-          <p class="description">Lower level and kitchen upgrades</p>
+          <h6>Kings Vision Grant</h6>
+          <p class="description">Structural Upgrades</p>
+        </div>
+      </div>
+      <div class="slidewrapper">
+        <div class="icon">
+          <img :src="'/images/poppy-flower.svg'">
+        </div>
+        <div class="text-wrapper">
+          <h6>Town of Wolfville</h6>
+          <p class="description">Structural Re-enforcement</p>
         </div>
       </div>
     </VueSlickCarousel>
@@ -59,8 +68,8 @@ export default {
     return {
       settings: {
         arrows: true,
-        slidesToShow: 4,
-        slidesToScroll: 4,
+        slidesToShow: 5,
+        slidesToScroll: 5,
         mobileFirst: true,
         responsive: [{
           breakpoint: 850,
